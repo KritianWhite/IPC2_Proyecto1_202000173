160f360ca1aa03e4170a3c4aeddbce90dbf6c82b
KritianWhite <88564832+KritianWhite@users.noreply.github.com>
2022-03-03 00:57:50 -0600

    Add files via upload

--- lista.py
+++ lista.py
@@ -1,26 +1,47 @@
+import webbrowser
+import os
 from tkinter import Label
 from tkinter.constants import ACTIVE, TRUE
+from turtle import shape
 from nodo import Nodo
 from graphviz import Digraph, Graph
 
 class lista:
+    '''
     def __init__(self) -> None:
         self.cabecera = None
 
-    def Colocar(self, articulo):
-        temp = Nodo(articulo)
+    def Insertar(self, x):
+        nuevoNodo = Nodo(x)
+        if self.cabecera == None:
+            self.cabecera = nuevoNodo
+        else:
+            auxiliarNodo = self.cabecera
+            while auxiliarNodo.siguiete is not None:
+                auxiliarNodo = auxiliarNodo.siguiete
+            auxiliarNodo.siguiete = nuevoNodo
+    
+    def impresion(self):
+        auxNodo = self.cabecera
+        while auxNodo is not None:
+            print(auxNodo.x)
+            auxNodo = auxNodo.siguiete
+    '''
+    
+    def Colocar(self, x):
+        temp = Nodo(x)
         temp.setSiguiente(self.cabecera)
         self.cabecera = temp
     
-    def Agregar(self, articulo):
+    def Agregar(self, x):
         if not self.cabecera:
-            self.cabecera = Nodo(articulo)
+            self.cabecera = Nodo(x)
             return
         correrPiso = self.cabecera
         while correrPiso.siguiete:
             correrPiso = correrPiso.siguiete
-        correrPiso.siguiete = Nodo(articulo)
-
+        correrPiso.siguiete = Nodo(x)
+    
     def tamanio(self):
         actual = self.cabecera
         contador = 0
@@ -32,12 +53,12 @@ class lista:
     
 
     
-    def remover(self, articulo):
+    def remover(self, x):
         actual = self.cabecera
         antes = None
         localizado = False
         while not localizado:
-            if actual.getPatron() == articulo:
+            if actual.getPatron() == x:
                 localizado = True
             else:
                 antes = actual
@@ -52,12 +73,12 @@ class lista:
         text = ''
         while actual != None:
             text += '|'
-            text += str(actual.getPatron())
+            text += str(actual.getPatron().costo)
             text += '|'
             actual = actual.getSiguiente()
         print(text)
 
-    def imprimirLista(self):
+    def imprimirMatriz(self):
         actual = self.cabecera
         while actual != None:
             actual.getPatron().imprimirCelda()
@@ -66,10 +87,43 @@ class lista:
     def imprimirLista(self):
         actual = self.cabecera
         while actual != None:
-            print(actual.getPatron())
+            actual.getPatron().imprimirCelda()
             actual = actual.getSiguiente()
 
-    #def graficarMatriz(self, nombre):
+    def graficaMatriz(self, nombre):
+        dot = Graph(comment='Pisos')
+        dot.attr(rankdir='LR')
+        actual = self.cabecera
+        Contador = 0
+        while actual != 0:
+            auxiliar = actual.getPatron().cabecera
+            Contador1 = 0
+            while auxiliar != None:
+                Contador += 1
+                Contador1 += 1
+                dot.node(str(Contador), str(auxiliar.getPatron().costo))
+                auxiliar = auxiliar.getSiguiente()
+                if Contador1 > 1:
+                    dot.edge(str(Contador - 1), str(Contador))
+                if Contador > (actual.getPatron().tamanio()):
+                    a = Contador - (actual.getPatron().tamanio())
+                    dot.edge(str(Contador), str(a), constraint = 'false')
+            actual = actual.getSiguiente()
+            dot.node('titulo', nombre+ ':', shape='plaintext', align='center')
+        dot.render(nombre, format='pdf', view=True)
+
+def graficaPisos(self, p):
+    actual = self.cabecera
+    cel = 1
+    while actual != None:
+        if cel == p:
+            actual.getPaton().celdas.graficaMatriz(actual.getPatron().nombre)
+            input('Se generó la gráfica.\n')
+            return
+        cel += 1
+        actual = actual.getSiguiente()
+    input('Ocurrio un error. \n')
+
 
 
     
--- mian.py
+++ mian.py
@@ -1,37 +1,102 @@
 from tkinter import filedialog
 import xml.etree.ElementTree as ET
 from lista import lista
+import ast
 
+class Posicion(object):
+    def __init__(self, x, y) -> None:
+        self.R = x
+        self.C = y
+        self.anterior = None 
+        self.siguiente = None
+        
 
-#class posicion(object):
-#    def __init__(self, x, y):
-#        self.x = x 
-#        self.y = y
-
-#class Celda(posicion):
-#    def __init__(self, x, y, costo):
-#        super().__init__(x, y)
-#        self.costo = costo
+class Celda(Posicion):
+    def __init__(self, x, y, costo) -> None:
+        super().__init__(x, y)
+        self.costo = costo
 
 class Pisos(object):
-    def __init__(self, nombre, r, c, f, s, celdas):
+    def __init__(self, nombre, filas, columnas, celdas, flipe, slide) -> None:
+        self.nombre = nombre
+        self.filas = filas
+        self.columnas = columnas 
+        self.celdas = celdas 
+        self.flipe = flipe 
+        self.slide = slide
+        self.siguiente = None 
+
+'''
+class Pisos(object):
+    def __init__(self, nombre = None, r = None, c = None, f = None, s = None):
         self.nombre = nombre
         self.row = r
         self.column = c
-        self.flipe = f 
-        self.slide = s
-        self.celdas = celdas
+        self.flipeCost = f 
+        self.slidecost = s
+        self.listapatrones = lista()
+        self.siguiente = None 
+        self.anterior = None
 
 class Patrones(Pisos):
-    def __init__(self, nombre, r, c, f, s, codigo, costo, patron, x, y):
+    def __init__(self, nombre=None, r=None, c=None, f=None, s=None, codigo = None, costo = None):
         super().__init__(nombre, r, c, f, s)
-        self.patron = patron
-        #self.codigo = codigo
+        self.codigo = codigo
+        self.costo = costo 
+
+class Patron(Patrones):
+    def __init__(self, nombre=None, r=None, c=None, f=None, s=None, codigo=None, costo=None, patrones = None):
+        super().__init__(nombre, r, c, f, s, codigo, costo)
+        self.patrones = patrones
+        self.siguiente = None
+'''
+
+##############
+
+'''
+class Posicion(Pisos):
+    def __init__(self, nombre, r, c, f, s):
+        super().__init__(nombre, r, c, f, s)
+    
+
+    #def __init__(self, R, C):
+    #    self.R = R 
+    #    self.C = C 
+
+class Celda(Posicion):
+    def __init__(self, R, C, Costo):
+        super().__init__(R, C)
+        self.Costo = Costo
+
+class Patron(Pisos):
+    def __init__(self, nombre, r, c, f, s, codigo, cadena):
+        super().__init__(nombre, r, c, f, s)
+        self.codigo = codigo 
+        self.cadena = cadena 
+
+class listaPatrones():
+    def __init__(self) -> None:
+        pass
+
+class Matriz():
+    def __init__(self) -> None:
+        pass
+
+#class Patrones(Pisos):
+#    def __init__(self, nombre, r, c, f, s, codigo, costo, patron, x, y):
+#        super().__init__(nombre, r, c, f, s)
+#       self.patron = patron
+#       self.codigo = codigo
         #self.costo = costo
         #self.x = x 
         #self.y = y 
+'''
+
 
 #-----------------MENU--------------------
+global arrayMatriz
+arrayMatriz = []
+
 if __name__ == "__main__":
     opcion = ''
     pisos = lista()
@@ -43,7 +108,7 @@ if __name__ == "__main__":
         print('2. Salir.')
         print('')
 
-        opcion = input('Ingrese un número del menú')
+        opcion = input('\nIngrese un número del menú:\n')
         if opcion == '1':
             pisos = lista()
             proceso = None
@@ -51,31 +116,87 @@ if __name__ == "__main__":
             extension = file.split('.')
 
             if extension[len(extension)-1] != 'xml' and extension[len(extension)-1] != 'XML':
-                input('\nArchivo con extensión incorrecta, presione enter para continuar.\n')
+                input('\nLa extensión del archivo es incorrecta, presione enter para continuar.\n')
                 continue
 
             documento = ET.parse(file)
             root = documento.getroot()
-            pisosLeidos = root.findall('pisosArtesanales')
-
+            #pisosLeidos = root.findall('piso')
+            #print(documento)
+            #print("hola")
             #---------LEENDO EL ARCHIVO--------------
-            for i in pisosLeidos:
-                #NOMBRE
-                nombre = i.attrib.get('nombre')
+            for pisos in root.iter("pisosArtesanales"):
+                for piso in root.iter("piso"):
+                
+                    #print("hola1")
+                    nombre = piso.attrib['nombre']
+                    #print('Nombre: {}'.format(nombre))
+
+                    for R,C,F,S, patrones in zip(piso.iter('R'), piso.iter('C'), piso.iter('F'), piso.iter('S'), piso.iter('patrones')):
+                        #print('R: {} C: {} F: {} S: {}'.format(R.text, C.text, F.text, S.text))
+                        for patron in patrones.iter('patron'):
+                            #print('Codigo: {} Patron: {}'.format(patron.attrib['codigo'],patron.text))
+                            patron = str(patron.text)
+                            print(patron)
+
+                            #MATRIZ
+                            #for celdas in range(len(patron)):
+                            for celdas in patron:
+                                arrayMatriz.append(celdas)
+                                print(arrayMatriz)
+                                #auxiliarmatriz = patron[celdas]
+                            #print(auxiliarmatriz)
+
+
+
+
+                            
+                            '''Celdas = lista()
+                            for matrizFila in range(1, int(R.text)+1):
+                                print('hola2')
+                                nuevaFila = lista()
+                                for matrizColumna in range (1, int(C.text)+1):
+                                    for posicion in patrones:
+                                        x1 = int(R.text)
+                                        y1 = int(C.text)
+                                        if x1 == matrizFila and y1 == matrizColumna:
+                                            costo = int(posicion.text)
+                                            nuevaCelda = Celda(x1,y1,costo)
+                                            nuevaFila.Agregar(nuevaCelda)
+                                Celdas.Agregar(nuevaFila)
+                                   '''          
+                '''
+                print(nombre)
                 #DIMENSIONES DEL AREA DE PISOS
                 dimensionPisos = i.findall('piso')
-                R = int((dimensionPisos[0].findall('R'))[0].text)
-                C = int((dimensionPisos[0].findall('C'))[0].text)
-                F = int(F.findall('F').text)
-                S = int(S.findall('S').text)
+                R = R
+                print(R)
+                #print(dimensionPisos)
+                #R = i.findall('R')
+                for j in i:
+                    R = j.findall('R')
+                    print(R)
+                    C = j.findall('C')
+                    print(C)
+                    F = j.findall('F')
+                    print(F)
+                    S = j.findall('S')
+                    print(S)
                 
                 #MATRIZ
-                patron = patron.findall('patron').text
-                posiciones = i.findall('posicion')
-                celdas = lista()
-                for j in range(1, R+1):
-                    newRow = lista()
-                    for k in range(1, C+1):
-                        for pos in posiciones:
-                            lista(patron[j][k])
-
+                #patron = i.text
+                #posiciones = i.findall('posicion')
+                #celdas = lista()
+                #for j in range(1, int(R+1)):
+                #    newRow = lista()
+                #    for k in range(1, int(C+1)):
+                #        for pos in posiciones:
+                #            lista(patron[j][k])
+                #        print(lista())
+                #        print('hola2')
+                '''
+        elif opcion == '2':
+            print('\nVuelva pronton..!\n')
+            break
+        else: 
+            input('\nElija una opcion válida. Presione enter para continuar.\n')
